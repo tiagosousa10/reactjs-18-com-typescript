a9bd0097071f2a36c0febe2c104f2ef3af8dd3f5
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-02 15:25:41 +0100

    props default

--- primeiro-projeto-vite/src/App.tsx
+++ primeiro-projeto-vite/src/App.tsx
@@ -8,9 +8,9 @@ const App = () => {
     return(
       <div>
         <h1>Meu primeiro componente</h1>
-        
+
         <UserInfo 
-          avatar="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F002%2F275%2F847%2Foriginal%2Fmale-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg&f=1&nofb=1&ipt=9231833f9668bbe67d8feb137ee5bf7b82587abdb3ba8cf9195876be365c669e&ipo=images  " 
+          avatar="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.freepik.com%2Fvector-premium%2Fdiseno-avatar-persona_24877-38131.jpg&f=1&nofb=1&ipt=de12729c38643ef01f764ff50b7f685b644f0fa6723d06f238c3095036d45089&ipo=images"
           name="Tiago"
           age={99} 
           email="teste@teste.com" 
@@ -18,7 +18,6 @@ const App = () => {
           />
         
         <UserInfo 
-          avatar="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F002%2F275%2F847%2Foriginal%2Fmale-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg&f=1&nofb=1&ipt=9231833f9668bbe67d8feb137ee5bf7b82587abdb3ba8cf9195876be365c669e&ipo=images  " 
           name="Pedro"
           age={23} 
           email="pedro@teste.com" 
--- primeiro-projeto-vite/src/components/UserInfo.tsx
+++ primeiro-projeto-vite/src/components/UserInfo.tsx
@@ -8,31 +8,31 @@ type Props = {
     name:string,
     email:string,
     age:number,
-    avatar:string,
+    avatar?:string,
     roles:{id:number,title:string}[]
 }
 
-export const UserInfo = (props:Props) => {
+export const UserInfo = ({name,email,age,roles,avatar = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F002%2F275%2F847%2Foriginal%2Fmale-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg&f=1&nofb=1&ipt=9231833f9668bbe67d8feb137ee5bf7b82587abdb3ba8cf9195876be365c669e&ipo=images'}:Props) => {
     return(
         <>
         <UserAvatar
-            src={props.avatar}
+            src={avatar}
         />
 
         <UserName
-            name={props.name}
+            name={name}
         />
 
         <UserEmail 
-            email={props.email}
+            email={email}
         />
 
         <UserAge 
-            age={props.age}
+            age={age}
         />
 
         <UserRoles 
-            roles={props.roles}
+            roles={roles}
         />
 
         </>
