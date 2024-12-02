3fd87103bbff0878e112cb14634f9c635b4d4f0d
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-02 01:38:34 +0100

    estruturas de exportacao/importacao

--- primeiro-projeto-vite/src/App.tsx
+++ primeiro-projeto-vite/src/App.tsx
@@ -1,10 +1,14 @@
+import React from "react"
+import UserAge ,{UserName ,UserEmail } from "./UserName"
+
 const App = () => {
   return(
     <div>
-      <h1>Meu componente</h1>
+      <h1>Meu componente de utilizador</h1>
+      <UserName/>
+      <UserEmail/>
+      <UserAge/>
     </div>
   )
 }
-
-
 export default App
