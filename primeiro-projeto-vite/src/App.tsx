3bce2554ae1c7c2b6623c34fb7113d3e807a55d3
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-02 21:01:13 +0100

    mouseEvents

--- primeiro-projeto-vite/src/App.tsx
+++ primeiro-projeto-vite/src/App.tsx
@@ -1,13 +1,17 @@
+import {MouseEvent} from 'react'
 import { Button } from "./components/Button"
 
 const App = () => {
-
-  const handleClick = (label:string) => alert(label)
-
+  const handleClick = (e: MouseEvent) => {
+    //interromper a propagacao
+    e.stopPropagation()
+    alert('cliquei no button')
+  }
   return(
-    <div>
-     <Button 
-      onClick={handleClick}/>
+    <div onClick={() => alert('Clicou na div')} style={{border: '1px solid red', padding:30, background: 'red'}}>
+      <button onClick={handleClick}>
+        Texto button!
+      </button>
     </div>
   )
  
