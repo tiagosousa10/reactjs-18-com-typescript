890cf43e82d3857c1339002063af4988b9287100
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-03 17:49:47 +0100

    editando elemento

--- primeiro-projeto-vite/src/App.tsx
+++ primeiro-projeto-vite/src/App.tsx
@@ -1,18 +1,25 @@
 import { useState } from "react"
 
 const App = () => {
-  const [tasks,setTasks] = useState<string[]>([])
+  const [tasks,setTasks] = useState<{label: string, done:boolean}[]>([])
   const [inputValue,setInputValue] = useState('')
  
   const handleAdd = () => {
     if(inputValue == '') return;
     //metodo 1
-    setTasks([...tasks, inputValue])
+    setTasks([...tasks, {label: inputValue, done:false}])
     setInputValue('')
   }
 
   const handleDel = (key:number) => {
     setTasks(tasks.filter((_value,index) => index != key))
+  }
+
+  const handleToggleDone = (key:number) => {
+    const newTasks = [...tasks]
+    newTasks[key].done = !newTasks[key].done
+    
+    setTasks(newTasks)
   }
 
   return(
@@ -31,7 +38,8 @@ const App = () => {
       <ul>
         {tasks.map((task, key) => (
           <li key={key}> 
-          {task}  
+          <input type="checkbox" checked={task.done} onChange={() => handleToggleDone(key)}/>
+          {task.label}  
           - 
           <button onClick={() => handleDel(key)}>Excluir</button> </li>
         ))}
