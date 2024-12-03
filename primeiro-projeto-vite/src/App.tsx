e7e2b0765f07437cb92e362d2b3e29d4976f8012
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-03 18:06:31 +0100

    monitorando dependencias do useeffect

--- primeiro-projeto-vite/src/App.tsx
+++ primeiro-projeto-vite/src/App.tsx
@@ -1,20 +1,16 @@
-import { useState } from "react"
 import { Card } from "./components/Card"
 
 
 const App = () => {
-  const [visible,setVisible] = useState(false)
-
-  const handleClick = () => setVisible(!visible)
+ 
  
 
   return(
     <div>
-      <button onClick={handleClick} >Mostrar / Ocultar</button>
-      {visible && (
+      
       <Card/>
 
-      )}
+   
     </div>
 
   )  
--- primeiro-projeto-vite/src/components/Card.tsx
+++ primeiro-projeto-vite/src/components/Card.tsx
@@ -1,16 +1,23 @@
-import {useEffect} from 'react'
+import { useState, useEffect } from "react"
 
 export const Card = () => {
+  const [firstName,setFirstName] = useState('')
+  const [lastName,setLastName] = useState('')
+  const [fullName,setFullName] = useState('')
+
   useEffect(() => {
-    console.log('card foi renderizado')
+    setFullName(`${firstName} ${lastName}`)
 
-    return () => console.log('card sumiu')
-  },[])
+    return () => console.log('useEffect já foi executado!')
+    
+  }, [firstName,lastName])
 
-  
   return(
     <div>
-      CARD!!!
+      <input type="text" value={firstName} onChange={e => setFirstName(e.target.value)} />
+      <input type="text" value={lastName} onChange={e =>setLastName(e.target.value)} />
+
+      <strong>Nome completo é: {fullName}</strong>
     </div>
   )
 }
