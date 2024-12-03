5c8b54a3c932fc87844cf06df670e26110d4659b
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-03 17:12:48 +0100

    states com objetos

--- primeiro-projeto-vite/src/App.tsx
+++ primeiro-projeto-vite/src/App.tsx
@@ -1,20 +1,26 @@
-import { useState } from "react"
+import { ChangeEvent, useState } from "react"
 
 const App = () => {
-  const [count,setCount] = useState(0)
+  const [fullName,setFullName] = useState({firstName: '', lastName: ''})
 
-  const handleClick = () => {
-   setCount(count + 1)
-   setCount(c => c + 1)
+  const handleChangeFirstName = (e:ChangeEvent<HTMLInputElement>) => {
+    setFullName({
+      ...fullName,
+      firstName: e.target.value
+    })
+  }
 
+  const handleChangeLastName = (e:ChangeEvent<HTMLInputElement>) => {
+    setFullName({
+      ...fullName,
+      lastName:e.target.value
+    })
   }
 
   return(
     <div>
-      <h2> {count} </h2>
-      <button onClick={handleClick}>
-        adicionar +1  
-        </button>
+      <input type="text" value={fullName.firstName} onChange={handleChangeFirstName} />
+      <input type="text" value={fullName.lastName} onChange={handleChangeLastName} />
     
     </div>
 
