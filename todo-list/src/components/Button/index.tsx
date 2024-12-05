c0f815ffc1bf2c55d6ea6f8932f3418432dd8346
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-05 22:46:19 +0100

    componenente Button

---
+++ todo-list/src/components/Button/index.tsx
@@ -0,0 +1,17 @@
+import styles from './Button.module.css'
+import AddIcon from '../../assets/add.svg?react'
+
+type Props = {
+  onClick : () => void
+}
+
+export const Button = ({onClick} : Props) => {
+  return(
+    <div className={styles.container}>
+      <button onClick={onClick}>
+        Criar <AddIcon/>
+      </button>
+
+    </div>
+  )
+}
