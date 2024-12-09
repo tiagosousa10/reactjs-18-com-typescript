0af2f0d8a6477d841f706ef5fb9e986a432caa10
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-09 22:43:02 +0100

    outra forma de criar rotas

--- react-router-vite/src/MainRoutes.tsx
+++ react-router-vite/src/MainRoutes.tsx
@@ -1,4 +1,4 @@
-import {Routes,Route, Outlet} from 'react-router-dom'
+import {Routes,Route, Outlet, useRoutes} from 'react-router-dom'
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Product } from './pages/Product';
@@ -7,7 +7,8 @@ import { NotFoundPage } from './pages/NotFoundPage';
 import { AuthMiddleware } from './middlewares/AuthMiddleware';
 import { Login } from './pages/Login';
 
-export const MainRoutes = () => {
+//metodo 1 - jsx
+/* export const MainRoutes = () => {
   return(
     <Routes>
     <Route path='*' element={<NotFoundPage/>} />
@@ -36,4 +37,44 @@ export const MainRoutes = () => {
     <Route path='/login' element={<Login/>} />     
   </Routes>
   )
+}
+ */
+
+
+//metodo 2 : objetos Javascript
+
+export const MainRoutes = () => {
+  const element = useRoutes([
+    {
+        index:true,
+        element: <Home/>
+    },
+    {
+      path: '/about',
+      element: <About/>
+    },
+    {
+      path: '/products',
+      element: <AuthMiddleware> <Outlet/> </AuthMiddleware>,
+      children: [
+        {
+          index:true,
+          element: <Products/>
+        },
+        {
+          path: ':categoryId/:productId',
+          element: <Product/>
+        }
+      ]
+    },
+    {
+      path: '/login',
+      element: <Login/>
+    },
+    {
+      path:'*',
+      element:<NotFoundPage/>
+    }
+])
+  return element;
 }
