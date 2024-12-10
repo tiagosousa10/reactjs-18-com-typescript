91042cdeffa6ae8845efcf920e042282ff509d4b
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-10 23:47:06 +0100

    criando e configurando useTheme

--- personal-finances/src/App.tsx
+++ personal-finances/src/App.tsx
@@ -1,12 +1,18 @@
 import { useEffect } from "react";
 import { useAuth } from "./hooks/auth";
+import { useTheme } from "./hooks/theme";
 
 const App = () => {
   const {handleAuthenticateUser} = useAuth()
+  const {handleInitTheme} = useTheme()
+
 
   useEffect(() => {
     // authenticate user using token saved in localstorage
     handleAuthenticateUser()
+
+    // apply theme saved by user to localstorage
+    handleInitTheme()
   }, [])
 
   return(
