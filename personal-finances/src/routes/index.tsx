1e2ac2817606a152d57825ac6618dbaddd7bc212
tiagosousa <tiagosousa.tams@hotmail.com>
2024-12-12 23:37:42 +0100

    css da page newTransaction

--- personal-finances/src/routes/index.tsx
+++ personal-finances/src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import { Auth } from "../pages/Auth"
 import { Layout } from "../components/Layout"
+import { Home } from "../pages/Home"
 
 export const MainRoutes = () => {
   return(
@@ -9,7 +10,7 @@ export const MainRoutes = () => {
       <Route path="/signup"  element={<Auth type="signup"/>}/>
       
       <Route element={<Layout/>}>
-       
+       <Route index element={<Home/>} />
       </Route>
     </Routes>
   )
